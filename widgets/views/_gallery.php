4ae1a073af34809591950aafc997928b6cfb69c6
Ruben Heymans <ruben.heymans@gmail.com>
2015-08-03 09:36:39 +0200

    Update widget

--- widgets/views/_gallery.php
+++ widgets/views/_gallery.php
@@ -1,4 +1,16 @@
 <?php
 use yii\helpers\Html;
 ?>
-<?= $model->name ?>
+<h2><?= $model->name ?></h2>
+
+<?= Html::img($model->getImage()->getUrl('600x'), ['class' => 'img-responsive center-block', 'alt' => $model->getImage()->alt, 'title' => $model->getImage()->title]) ?>
+
+<p><?= $model->description ?></p>
+
+<div class="row">
+    <?php foreach ($model->getImages() as $image): ?>
+    <div class="col-sm-6">
+        <?= Html::img($image->getUrl('200x200'), ['class' => 'img-responsive center-block thumbnail', 'alt' => $image->alt, 'title' => $image->title]) ?>
+    </div>
+    <?php endforeach; ?>
+</div>
