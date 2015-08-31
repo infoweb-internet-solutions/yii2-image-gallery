11719072214bda2531f9fa93a3573e2aceaad8f5
Ruben Heymans <ruben.heymans@gmail.com>
2015-08-31 11:24:56 +0200

    Update translations

--- messages/nl/infoweb/gallery.php
+++ messages/nl/infoweb/gallery.php
@@ -1,14 +1,16 @@
 <?php
 
 return [
-    'Drag & drop files here …' => 'Sleep hier bestanden naartoe ...',
+    'Drag & drop files here …' => 'Sleep hier je bestanden naartoe ...',
     'Galleries' => 'Fotoalbums',
     'Gallery' => 'Fotoalbum',
     'Not uploaded yet' => 'Nog niet ge-upload',
     'Remove file' => 'Verwijder bestand',
+    'Thumbnail height' => '',
+    'Thumbnail width' => '',
     'Upload Error' => 'Upload error',
     'Upload file' => '',
     'Uploaded' => 'Ge-upload',
     'Uploading ...' => 'Uploading ...',
-    'gallery' => 'fotoalbum',
+    'gallery' => '',
 ];
