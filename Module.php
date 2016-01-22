eb4faf17baed8f22899b426c94874709bc61277a
Ruben Heymans <ruben.heymans@gmail.com>
2016-01-22 09:45:06 +0100

    Add duplicate language and update migration

--- Module.php
+++ Module.php
@@ -6,8 +6,19 @@ use Yii;
 
 class Module extends \yii\base\Module
 {
+    /**
+     * Allow content duplication with the "duplicateable" plugin
+     * @var boolean
+     */
+    public $allowContentDuplication = true;
+
     public function init()
     {
         parent::init();
+
+        // Content duplication is only possible if there is more than 1 app language
+        if (isset(Yii::$app->params['languages']) && count(Yii::$app->params['languages']) == 1) {
+            $this->allowContentDuplication = false;
+        }
     }
 }
--- views/gallery/_default_tab.php
+++ views/gallery/_default_tab.php
@@ -1,18 +1,21 @@
 <?php
 
 use yii\bootstrap\Tabs;
+use kartik\icons\Icon;
+use infoweb\cms\helpers\LanguageHelper;
 
 // Add the language tabs
 foreach (Yii::$app->params['languages'] as $languageId => $languageName) {
     $tabs[] = [
-        'label' => $languageName,
+        'label' => $languageName . ((Yii::$app->getModule('gallery')->allowContentDuplication) ? Icon::show('exchange', ['class' => 'duplicateable-all-icon not-converted', 'data-language' => $languageId]) : ''),
         'content' => $this->render('_default_language_tab', ['model' => $model->getTranslation($languageId), 'form' => $form]),
+        'options' => ['class' => (LanguageHelper::isRtl($languageId)) ? 'rtl' : ''],
     ];
 }
 
 ?>
 <div class="tab-content default-tab">
 
-    <?= Tabs::widget(['items' => $tabs]); ?>
+    <?= Tabs::widget(['items' => $tabs, 'encodeLabels' => false]); ?>
 
 </div>
