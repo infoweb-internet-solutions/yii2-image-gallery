5a81faca3db794300d6d10b83a132a712df689a9
Ruben Heymans <ruben.heymans@gmail.com>
2016-01-22 14:50:57 +0100

    Update widget

--- widgets/views/_list.php
+++ widgets/views/_list.php
@@ -1,13 +1,19 @@
 <?php
 use yii\helpers\Html;
 ?>
-<?php foreach ($models as $model): ?>
-<?php if ($model->getImage(false)): ?>
-<div class="<?= $class ?>">
-    <a href="<?= $model->getUrl(false) ?>">
-        <?= Html::img($model->getImage(false)->getUrl('260x170'), ['class' => 'img-responsive center-block', 'alt' => $model->getImage()->alt, 'title' => $model->getImage()->title]) ?>
-        <?= $model->name ?>
-    </a>
+<?php if ($models): ?>
+<div class="row">
+    <?php foreach ($models as $model): ?>
+    <?php if ($model->getImage(false)): ?>
+    <div class="<?= $class ?>">
+        <div class="title"><?= $model->name ?></div>
+        <?= Html::a(
+            Html::img($model->getImage(false)->getUrl('400x250'), ['class' => 'img-responsive center-block', 'alt' => $model->getImage()->alt, 'title' => $model->getImage()->title]),
+            $model->getUrl(false),
+            ['class' => 'thumbnail']
+        ) ?>
+    </div>
+    <?php endif; ?>
+    <?php endforeach; ?>
 </div>
 <?php endif; ?>
-<?php endforeach; ?>
